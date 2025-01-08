f5cf0a28b50e83e82c529b1fbcbe585c1d691e36
HP <aditidevani66@gmail.com>
2025-01-08 10:38:36 +0530

    product secma

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,57 +1,175 @@
-const mongoose = require("mongoose")
+// productSchema.js
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        trim: true
+        // trim: true
     },
     slug: {
         type: String,
         required: true,
-        unique: true,
-        lowercase: true
+        // unique: true,
+        // lowercase: true
     },
     description: {
         type: String,
-        required: true,
+        required: true
     },
     price: {
-        type: Number,
-        required: true,
+        actualPrice: {
+            type: Number,
+            required: true
+        },
+        salePrice: {
+            type: Number
+        },
     },
     category: {
         type: String,
-        // required: true,
         ref: "Category"
+    },
+    subCategory: {
+        type: String
+    },
+    productType: {
+        type: String
     },
     brand: {
         type: String,
-        enum: ["Apple", "Samsung", "Lenovo"],
+        required: true,
     },
     quantity: {
         type: Number,
-        required: true,
+        required: true
     },
     sold: {
         type: Number,
-        default: 0,
+        default: 0
     },
     images: {
-        type: Array,
+        type: Array
     },
-    color: {
-        type: String,
-        required: true,
+    colors: [
+        {
+            colorName: {
+                type: String
+            },
+            images: [
+                {
+                    type: String
+                }
+            ],
+            stock: {
+                type: Number
+            },
+            isActive: {
+                type: Boolean
+            }
+        }
+    ],
+    sizes: [
+        {
+            size: {
+                type: String
+            },
+            stock: {
+                type: Number
+            }
+        }
+    ],
+    thumbnail: {
+        type: String
     },
-    ratings: [{
-        star: Number,
-        postedby: {type:mongoose.Schema.Types.ObjectId, ref: "User"}
+    gallery: [
+        {
+            type: String
+        }
+    ],
+    rating: {
+        average: {
+            type: Number
+        },
+        count: {
+            type: Number
+        }
     },
-    ]
-}, {
-    timestamps: true
-})
+    offers: [
+        {
+            type: {
+                type: String
+            },
+            details: {
+                type: String
+            }
+        }
+    ],
+    availability: {
+        isInStock: {
+            type: Boolean
+        },
+        deliveryOptions: {
+            standardDelivery: {
+                type: String
+            },
+            fastestDelivery: {
+                type: String
+            }
+        }
+    },
+    reviews: [
+        {
+            user: {
+                type: String
+            },
+            rating: {
+                type: Number
+            },
+            comment: {
+                type: String
+            },
+            date: {
+                type: String
+            }
+        }
+    ],
+    gender: {
+        type: String
+    },
+    ASIN: {
+        type: String
+    },
+    dimensions: {
+        height: {
+            type: Number
+        },
+        width: {
+            type: Number
+        },
+        depth: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        }
+    },
+    shippingDetails: {
+        shippingWeight: {
+            type: Number
+        },
+        dimensions: {
+            height: {
+                type: Number
+            },
+            width: {
+                type: Number
+            },
+            depth: {
+                type: Number
+            }
+        },
+    }
+}, { timestamps: true });
 
-
-module.exports = mongoose.model('Product',productSchema)
+module.exports = mongoose.model('Product', productSchema);
